6afd6a8158cc8efe6dea7844dd4d42c926bc3199
Murali <muralikrishna1986@gmail.com>
2020-09-10 03:58:42 +0800

    Second Commit

---
+++ src/main/java/SecondClass.java
@@ -0,0 +1,7 @@
+public class SecondClass {
+    public static void main(String[] args){
+        System.out.println("Testing second code in intellij");
+        System.out.println("it is working");
+        FirstClass.TestMethod(10,20);
+    }
+}
